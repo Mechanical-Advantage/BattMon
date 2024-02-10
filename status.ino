2e72dff0386e2795827a4534051b7d95921c44e4
Jaden Chen <chenjaden3399@gmail.com>
2024-02-10 16:12:40 -0500

    Removed extraneous comments

--- status.ino
+++ status.ino
@@ -4,27 +4,7 @@
 #define DATA_PIN 13
 #define LIGHT_SENSOR_PIN A0
 
-
-
 CRGB leds[NUM_LEDS];
-
-//void setup() { FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS); }
-//void loop() {
-//  
-//  for(int i=0; i<NUM_LEDS; i++){
-//    leds[i] = CRGB::Red; 
-//  }
-//  FastLED.show(); 
-//  delay(30);
-//  
-//  for(int i=0; i<NUM_LEDS; i++){
-//    leds[i] = CRGB::Blue; 
-//  }
-//  
-//  FastLED.show(); 
-//  delay(30);
-//}
-
 
 void setup() {
   // Setup LEDs
