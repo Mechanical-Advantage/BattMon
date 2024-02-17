fbb6be28929c3ed8c6599e8685b55152f1f76df2
Jaden Chen <chenjaden3399@gmail.com>
2024-02-17 15:31:37 -0500

    Added support for multiple sensors/leds

--- status.ino
+++ status.ino
@@ -1,17 +1,26 @@
 #include <FastLED.h>
 
-#define NUM_LEDS 2
-#define DATA_PIN 13
-#define LIGHT_SENSOR_PIN A0
+#define NUM_LEDS 3
+#define NUM_SENSORS 3
+
+#define DATA_PIN 10
+
+#define LIGHT_SENSOR_PIN_0 A15
+#define LIGHT_SENSOR_PIN_1 A14
+#define LIGHT_SENSOR_PIN_2 A13
+
 
 CRGB leds[NUM_LEDS];
+uint8_t light_sensor_pins[NUM_SENSORS] = {A15, A14, A13};
 
 void setup() {
   // Setup LEDs
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 
-  // Setup light sensor
-  pinMode(LIGHT_SENSOR_PIN, INPUT);
+  for(int i = 0; i<NUM_SENSORS; i++) {
+    // Setup light sensor
+    pinMode(light_sensor_pins[i], INPUT);
+  }
 
   //Setup console
   Serial.begin(9600); //begin Serial Communication
@@ -19,23 +28,22 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-
-  int raw_light = analogRead(LIGHT_SENSOR_PIN); // read the raw value from light_sensor pin (A3)
-  int light = map(raw_light, 0, 1023, 0, 100); // map the value from 0, 1023 to 0, 100
- 
-  Serial.print("Light level: "); 
-  Serial.println(light); // print the light value in Serial Monitor
-  
-  if (light>60){
-    leds[0] = CRGB::Green;
-    FastLED.show();
-    delay(30);
+  for(int i=0; i<NUM_SENSORS; i++)
+  {
+    int raw_light = analogRead(light_sensor_pins[i]); // read the raw value from light_sensor pin (A3)
+    int light = map(raw_light, 0, 1023, 0, 100); // map the value from 0, 1023 to 0, 100
+    Serial.print(String(light_sensor_pins[i]));
+    Serial.println("Sensor [" +  String(i) + "]" + String(light)); 
+    
+    if (light>45){
+      leds[i] = CRGB::Purple;
+      delay(30);
+    }
+    else {
+      leds[i] = CRGB::Black;
+      delay(30);
+    }
   }
-  else {
-    leds[0] = CRGB::Black;
-    FastLED.show();
-    delay(30);
-  }
-  
+  FastLED.show();
 }
   
